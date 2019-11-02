307594142dba76ac8bf4ca549ae338d3137a3a55
stembest <57277257+stembest@users.noreply.github.com>
2019-11-02 14:33:06 -0600

    Update main.c

---
+++ main.c
@@ -0,0 +1,6 @@
+task main() //This is the task that is called when the robot starts the program.
+{
+	while(true) { //This is the main loop, everything that is updated continuously will go in here.
+
+	}
+}
